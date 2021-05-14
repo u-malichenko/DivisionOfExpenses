6d9aa5f8aa09bacb8a3ea98403b49619736886d7
palandarus <klarkken@yandex.ru>
2021-05-14 16:27:09 +0300

    начал добавлять репозитории, сервис и контроллеры для сущностей

---
+++ src/main/java/ru/division/of/expenses/app/controller/OrdersController.java
@@ -0,0 +1,9 @@
+package ru.division.of.expenses.app.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@RequiredArgsConstructor
+public class OrdersController {
+}
