4be4b9bc4dd50befb22f365533f15f2430c0cb94
u-malichenko <u.malichenko@gmail.com>
2021-05-17 12:19:06 +0300

    Прячем настройки подключения к БД

--- src/main/java/ru/division/of/expenses/app/DivisionOfExpensesApplication.java
+++ src/main/java/ru/division/of/expenses/app/DivisionOfExpensesApplication.java
@@ -2,8 +2,10 @@ package ru.division.of.expenses.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:secured.properties")
 public class DivisionOfExpensesApplication {
 
 	public static void main(String[] args) {
