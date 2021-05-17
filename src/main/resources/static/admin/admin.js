917ab68ffaad2fb0d5b3f3924512de3391c77c13
u-malichenko <u.malichenko@gmail.com>
2021-05-17 11:57:33 +0300

    front первый запуск главной страницы

--- src/main/resources/static/admin/admin.js
+++ src/main/resources/static/admin/admin.js
@@ -1,11 +1,11 @@
-angular.module('app').controller('adminController', function ($scope, $http) {
-    const contextPath = 'http://localhost:8189/market';
-
-    $scope.submitCreateNewProduct = function () {
-        $http.post(contextPath + '/api/v1/products', $scope.newProduct)
-            .then(function (response) {
-                $scope.newProduct = null;
-                console.log(response.data());
-            });
-    };
-});
+// angular.module('app').controller('adminController', function ($scope, $http) {
+//     const contextPath = 'http://localhost:8189/market';
+//
+//     $scope.submitCreateNewProduct = function () {
+//         $http.post(contextPath + '/api/v1/products', $scope.newProduct)
+//             .then(function (response) {
+//                 $scope.newProduct = null;
+//                 console.log(response.data());
+//             });
+//     };
+// });
--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -11,60 +11,60 @@
             .when('/', {
                 templateUrl: 'main/main.html'
             })
-            .when('/auth', {
-                templateUrl: 'auth/auth.html',
-                controller: 'authController'
-            })
-            .when('/store', {
-                templateUrl: 'store/store.html',
-                controller: 'storeController'
-            })
-            .when('/admin', {
-                templateUrl: 'admin/admin.html',
-                controller: 'adminController'
-            })
-            .when('/cart', {
-                templateUrl: 'cart/cart.html',
-                controller: 'cartController'
-            })
-            .when('/order', {
-                templateUrl: 'order/order.html',
-                controller: 'orderController'
-            })
-            .when('/orders', {
-                templateUrl: 'orders/orders.html',
-                controller: 'ordersController'
-            })
-            .when('/profile', {
-                templateUrl: 'profile/profile.html',
-                controller: 'profileController'
-            })
+            // .when('/auth', {
+            //     templateUrl: 'auth/auth.html',
+            //     controller: 'authController'
+            // })
+            // .when('/store', {
+            //     templateUrl: 'store/store.html',
+            //     controller: 'storeController'
+            // })
+            // .when('/admin', {
+            //     templateUrl: 'admin/admin.html',
+            //     controller: 'adminController'
+            // })
+            // .when('/cart', {
+            //     templateUrl: 'cart/cart.html',
+            //     controller: 'cartController'
+            // })
+            // .when('/order', {
+            //     templateUrl: 'order/order.html',
+            //     controller: 'orderController'
+            // })
+            // .when('/orders', {
+            //     templateUrl: 'orders/orders.html',
+            //     controller: 'ordersController'
+            // })
+            // .when('/profile', {
+            //     templateUrl: 'profile/profile.html',
+            //     controller: 'profileController'
+            // })
             .otherwise({
                 redirectTo: '/'
             });
 
-        $httpProvider.interceptors.push(function ($q, $location) {
-            return {
-                'responseError': function (rejection, $localStorage, $http) {
-                    var defer = $q.defer();
-                    if (rejection.status == 401 || rejection.status == 403) {
-                        console.log('error: 401-403!');
-                        $location.path('/auth');
-                        if (!(localStorage.getItem("localUser") === null)) {
-                            delete $localStorage.currentUser;
-                            $http.defaults.headers.common.Authorization = '';
-                            console.log('zxc');
-                        }
-                        console.log(rejection.data);
-                        var answer = JSON.parse(rejection.data);
-                        console.log(answer);
-                        // window.alert(answer.message);
-                    }
-                    defer.reject(rejection);
-                    return defer.promise;
-                }
-            };
-        });
+        // $httpProvider.interceptors.push(function ($q, $location) {
+        //     return {
+        //         'responseError': function (rejection, $localStorage, $http) {
+        //             var defer = $q.defer();
+        //             if (rejection.status == 401 || rejection.status == 403) {
+        //                 console.log('error: 401-403!');
+        //                 $location.path('/auth');
+        //                 if (!(localStorage.getItem("localUser") === null)) {
+        //                     delete $localStorage.currentUser;
+        //                     $http.defaults.headers.common.Authorization = '';
+        //                     console.log('zxc');
+        //                 }
+        //                 console.log(rejection.data);
+        //                 var answer = JSON.parse(rejection.data);
+        //                 console.log(answer);
+        //                 // window.alert(answer.message);
+        //             }
+        //             defer.reject(rejection);
+        //             return defer.promise;
+        //         }
+        //     };
+        // });
     }
 
     function run($rootScope, $http, $localStorage) {
@@ -83,7 +83,7 @@
 })();
 
 angular.module('app').controller('indexController', function ($scope, $http, $localStorage) {
-    const contextPath = 'http://localhost:8189/market';
+    const contextPath = 'http://localhost:8189/doe';
 
     $scope.isUserLoggedIn = function () {
         if ($localStorage.currentUser) {
