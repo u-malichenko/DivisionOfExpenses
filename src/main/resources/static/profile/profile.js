75f5c5954824b67688e31da4a8f074c2f382f9b9
u-malichenko <u.malichenko@gmail.com>
2021-05-17 10:53:14 +0300

    front init

---
+++ src/main/resources/static/profile/profile.js
@@ -0,0 +1,25 @@
+angular.module('app').controller('profileController', function ($scope, $http) { //, jwtHelper
+    const contextPath = 'http://localhost:8189/market';
+
+    $scope.updateProfile = function () {
+         $http.put(contextPath + '/api/v1/profile', $scope.profile)
+            .then(function () {
+                $scope.loadProfile();
+                window.alert("Профиль обновлен");
+            });
+    };
+
+    $scope.loadProfile = function () {
+        $http.get(contextPath + '/api/v1/profile')
+        .then(function (response) {
+            $scope.profile = response.data;
+        });
+    };
+
+    // $scope.getUserRole = function() {
+    //     let tokenPayload = jwtHelper.decodeToken($localStorage.currentUser.token);
+    //     $scope.userRoles = tokenPayload.roles;
+    // };
+
+    $scope.loadProfile();
+});
