2e506eaaa0d6bc07b618d62f2dcb59cee113f542
u-malichenko <u.malichenko@gmail.com>
2021-05-17 14:08:45 +0300

    подготовил каркасы страниц редактирования профиля и страницы авторизации НУЖЕН БЭК!

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -11,10 +11,10 @@
             .when('/', {
                 templateUrl: 'main/main.html'
             })
-            // .when('/auth', {
-            //     templateUrl: 'auth/auth.html',
-            //     controller: 'authController'
-            // })
+            .when('/auth', {
+                templateUrl: 'auth/auth.html',
+                controller: 'authController'
+            })
             // .when('/store', {
             //     templateUrl: 'store/store.html',
             //     controller: 'storeController'
@@ -44,27 +44,27 @@
             });
 
         // $httpProvider.interceptors.push(function ($q, $location) {
-        //     return {
-        //         'responseError': function (rejection, $localStorage, $http) {
-        //             var defer = $q.defer();
-        //             if (rejection.status == 401 || rejection.status == 403) {
-        //                 console.log('error: 401-403!');
-        //                 $location.path('/auth');
-        //                 if (!(localStorage.getItem("localUser") === null)) {
-        //                     delete $localStorage.currentUser;
-        //                     $http.defaults.headers.common.Authorization = '';
-        //                     console.log('zxc');
-        //                 }
-        //                 console.log(rejection.data);
-        //                 var answer = JSON.parse(rejection.data);
-        //                 console.log(answer);
-        //                 // window.alert(answer.message);
-        //             }
-        //             defer.reject(rejection);
-        //             return defer.promise;
-        //         }
-        //     };
-        // });
+        //         //     return {
+        //         //         'responseError': function (rejection, $localStorage, $http) {
+        //         //             var defer = $q.defer();
+        //         //             if (rejection.status == 401 || rejection.status == 403) {
+        //         //                 console.log('error: 401-403!');
+        //         //                 $location.path('/auth');
+        //         //                 if (!(localStorage.getItem("localUser") === null)) {
+        //         //                     delete $localStorage.currentUser;
+        //         //                     $http.defaults.headers.common.Authorization = '';
+        //         //                     console.log('zxc');
+        //         //                 }
+        //         //                 console.log(rejection.data);
+        //         //                 var answer = JSON.parse(rejection.data);
+        //         //                 console.log(answer);
+        //         //                 // window.alert(answer.message);
+        //         //             }
+        //         //             defer.reject(rejection);
+        //         //             return defer.promise;
+        //         //         }
+        //         //     };
+        //         // });
     }
 
     function run($rootScope, $http, $localStorage) {
