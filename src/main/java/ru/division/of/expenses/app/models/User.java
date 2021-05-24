3e2125f7d577ab0d974ba5160be7cf92fd6b5a6a
palandarus <klarkken@yandex.ru>
2021-05-24 16:35:30 +0300

    добавил скрипты скрипты для заполнения евентов, трат и участников трат

--- src/main/java/ru/division/of/expenses/app/models/User.java
+++ src/main/java/ru/division/of/expenses/app/models/User.java
@@ -26,8 +26,8 @@ public class User extends AbstractEntity {
     @ManyToMany
     @JoinTable(
             name = "users_roles",
-            joinColumns = @JoinColumn(name = "role_id"),
-            inverseJoinColumns = @JoinColumn(name = "user_id"))
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
     private Collection<Role> roles = new ArrayList<>();
 
 
