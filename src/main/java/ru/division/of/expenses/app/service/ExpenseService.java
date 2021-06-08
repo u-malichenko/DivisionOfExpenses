64f1a5ff7df6252345d5b99ee7cf4477a078724e
listpv <listpv@mail.ru>
2021-06-08 20:05:32 +0300

    Изменения в маппер Трат, корректировка

--- src/main/java/ru/division/of/expenses/app/service/ExpenseService.java
+++ src/main/java/ru/division/of/expenses/app/service/ExpenseService.java
@@ -77,7 +77,10 @@ public class ExpenseService {
 
     public ResponseEntity<?> updateExpense(ExpenseDto expenseDto) {
         Expense expense = mappingExpenseDtoToExpenseUtils.mapToExpense(expenseDto);
-        return updateExpense(expense);
+        if(expenseRepository.save(expense) != null){
+            return new ResponseEntity<String>("Expense was successfully updated", HttpStatus.ACCEPTED);
+        }
+        return new ResponseEntity<EmptyJsonResponse>(new EmptyJsonResponse(), HttpStatus.NOT_ACCEPTABLE);
     }
 
     private Expense findExpenseByIdBasic(Long id) {
